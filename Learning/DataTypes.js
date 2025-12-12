7828252bd1dd07155a389a25c50ee5447da3c26b
Vivekanandd1 <vivekanandd1@live.com>
2025-12-12 15:07:06 +0530

    DataTypes

---
+++ Learning/DataTypes.js
@@ -0,0 +1,19 @@
+let Fname = "Vatsal"
+console.log(typeof Fname)
+console.log(Fname)
+
+let MobileNo = 8109772504
+console.log(typeof MobileNo)
+console.log(MobileNo)
+
+let Status = false
+console.log(typeof Status)
+console.log(Status)
+
+let State = null
+console.log(typeof State)
+console.log(State)
+
+let Arr ={}
+console.log(typeof Arr)
+console.log(Arr)
