a3487e351a2b8ed78250728f8d52c9c2c3b8fbe0
Vivekanandd1 <vivekanandd1@live.com>
2026-01-07 17:35:41 +0530

    Allure Integration

--- playwright.config.js
+++ playwright.config.js
@@ -20,14 +20,17 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  // retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 0,
   /* Retry on Headed/unHeaded mode on local environment */
-  retries: 1,
+  // retries: 1,
 
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    ['allure-playwright']
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -45,15 +48,16 @@ export default defineConfig({
       screenshot:"on",
       video:"on",
       trace:"on"
-
-      },
- 
+      }
     },
 
     // {
     //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    //   screenshot:on,
+    //   use: { ...devices['Desktop Firefox'],
+    //   screenshot:"on",
+    //   video:"on",
+    //   trace:"on"
+    //   }
     // },
 
     // {
